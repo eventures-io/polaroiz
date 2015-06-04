b430b1a8a5344c9c0cbdbf3c56ab94875f490d84
Peter Symoens <peter.symoens@eventures.io>
2015-06-04 13:09:34 +0100

    add flickr service

---
+++ app/scripts/flickr-resource.js
@@ -0,0 +1,53 @@
+'use strict'
+
+angular.module('polaroiz').factory('FlickrResource', function ($http, $q) {
+
+    var findPictures = function (subject) {
+
+        var url = 'https://api.flickr.com/services/rest/?format=json&nojsoncallback=?&api_key=db1853e3b042d27e15b301da2b789f4e';
+        var query = '&method=flickr.photosets.getPhotos&photoset_id=72157642179604694';
+
+        function Picture(id, title, url, owner, ownerName) {
+            this.id = id,
+                this.title = title,
+                this.url = url ,
+                this.owner = owner,
+                this.ownerName = ownerName
+
+        }
+
+        var defered = $q.defer();
+
+         $http.get(url + query).
+            success(function (data, status, headers, config) {
+
+                var result = [];
+
+                if (data.stat === 'ok') {
+                    angular.forEach(data.photoset.photo, function (value, key) {
+                        var url = 'https://farm' + value.farm + '.staticflickr.com/' + value.server + '/' + value.id + '_' + value.secret + '_m.jpg';
+                        this.push(new Picture(value.id, value.title, url, data.photoset.owner, data.photoset.ownerName));
+                    }, result)
+
+                } else {
+                    defered.reject('No data received');
+                }
+                defered.resolve(result)
+            }).
+            error(function (data, status, headers, config) {
+                $log.error('error loading pictures: ' + JSON.stringify(status));
+                 defered.reject(status);
+            });
+
+        return defered.promise;
+
+    }
+
+    return {
+
+        findPictures: findPictures
+
+    }
+
+
+})
--- app/scripts/zoom-target.js
+++ app/scripts/zoom-target.js
@@ -2,34 +2,41 @@
 
 angular.module('polaroiz').directive('zoomTarget', function () {
 
+    function getRandomInt(min, max) {
+        return Math.floor(Math.random() * (max - min)) + min;
+    }
+
     return {
         restrict: 'E',
         replace: true,
         template: '<div class="polaroid-outer" ng-click="click()"><div class="polaroid-inner"></div></div>',
+        scope: {
+        },
         link: function (scope, element, attrs) {
             element.zoomTarget();
             element.addClass('zoomTarget');
+
             element.css('position', 'absolute');
-            element.css('left', getRandomInt(0, 500) + 'px');
-            element.css('top', getRandomInt(0, 500) + 'px');
-            var rotation = getRandomInt(0, 90);
+            element.css('left', getRandomInt(0, 1280) + 'px');
+            element.css('top', getRandomInt(0, 600) + 'px');
+
+            var rotation = getRandomInt(-90, 90);
             element.css('-webkit-transform', 'rotate(' + rotation + 'deg)');
             element.css('-moz-transform', 'rotate(' + rotation + 'deg)');
             element.css('-o-transform', 'rotate(' + rotation + 'deg)');
-            scope.picture = attrs.content;
+
+            var picture = scope.$eval(attrs.content);
+            element.find('.polaroid-inner').append('<img src="' + picture.url + '" class="polaroid-image">');
+
         },
-        controller: function($scope, $element){
-            $scope.click = function(){
+        controller: function ($scope, $element) {
+            $scope.click = function () {
                 $element.zoomTo({targetsize: 0.7});
-                $('.polaroid-outer').not(this).css({'z-index':1});
+                $('.polaroid-outer').not(this).css({'z-index': 1});
                 $element.css('z-index', 2);
-                $scope.apply();
-            }
+            };
         }
     };
 
-    function getRandomInt(min, max) {
-        return Math.floor(Math.random() * (max - min)) + min;
-    }
 
 });
