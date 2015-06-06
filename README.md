76c234fc7103b41dc980743c550e3984251d9fbf
Peter Symoens <peter.symoens@eventures.io>
2015-06-06 14:20:15 +0100

    add mouse listener

--- README.md
+++ README.md
@@ -4,4 +4,6 @@
 An AngularJS version of the Zoomooz picture gallery
 https://github.com/jaukia/zoomooz
 
+Deployed on heroku:
 
+https://polaroiz.herokuapp.com
--- client/app/scripts/zoom-target.js
+++ client/app/scripts/zoom-target.js
@@ -17,53 +17,46 @@ angular.module('polaroiz').directive('zoomTarget', function () {
             element.addClass('zoomTarget');
             element.css('position', 'absolute');
 
-//    var img = $('<img id="dynamic">'); //Equivalent: $(document.createElement('img'))
-//    img.attr('src', responseObject.imgurl);
-//    img.appendTo('#imagediv');
+            var picture = scope.$eval(attrs.content);
+            var img = $('<img class="polaroid-image">').attr('src', picture.url)
+                .load(function () {
+                    element.css('height', this.naturalHeight / 4);
+                    element.css('width', this.naturalWith / 4);
 
-//    myImage.addEventListener('onload', function() {
-//        console.log('My width is: ', this.naturalWidth);
-//        console.log('My height is: ', this.naturalHeight);
-//    });
-                var picture = scope.$eval(attrs.content);
-                var img = $('<img class="polaroid-image">').attr('src', picture.url)
-                    .load(function() {
-                        //$log.debug(this.naturalWidth +  ' ' + this.naturalHeight)
-                        element.css('height', this.naturalHeight / 4);
-                        element.css('width',this.naturalWith / 4);
+                    var viewWith = $(document).width() - 200;
+                    element.css('left', getRandomInt(170, viewWith) + 'px');
 
-                        var viewWith = $( document ).width() - 200;
-                        element.css('left', getRandomInt(170, viewWith) + 'px');
+                    var viewHeight = ($(document).height() * 90 / 100) - 200;
+                    element.css('top', getRandomInt(80, viewHeight) + 'px');
 
-                        var viewHeight = ($( document ).height() * 90 /100) - 200;
-                        element.css('top', getRandomInt(80, viewHeight) + 'px');
+                    var rotation = getRandomInt(-90, 90);
+                    element.css('-webkit-transform', 'rotate(' + rotation + 'deg)');
+                    element.css('-moz-transform', 'rotate(' + rotation + 'deg)');
+                    element.css('-o-transform', 'rotate(' + rotation + 'deg)');
 
-                        var rotation = getRandomInt(-90, 90);
-                        element.css('-webkit-transform', 'rotate(' + rotation + 'deg)');
-                        element.css('-moz-transform', 'rotate(' + rotation + 'deg)');
-                        element.css('-o-transform', 'rotate(' + rotation + 'deg)');
+                    element.find('.polaroid-inner').append(this);
 
-                        element.find('.polaroid-inner').append(this);
-
-
-                    });
-
-
-
-
-
-
+                });
 
         },
-        controller: function ($scope, $element) {
+        controller: function ($scope, $element, $log) {
+
+            var handlerIn =  function() {
+                $log.debug('mouse enter');
+            }
+
+            var handlerOut =  function() {
+                $log.debug('mouse leave');
+            }
+
             $scope.click = function () {
-                if($element.css('z-index') !== '2'){
-                $('.polaroid-outer').not(this).css({'z-index': 1});
-                $element.css('z-index', 2);
+                if ($element.css('z-index') !== '2') {
+                    $('.polaroid-outer').not(this).css({'z-index': 1});
+                    $('.polaroid-outer').not(this).mouseenter(null).mouseleave(null);
+                    $element.css('z-index', 2);
+                    $element.mouseenter( handlerIn ).mouseleave( handlerOut );
                 }
-                else {
-                    //alert('on top');
-                }
+
             };
         }
     };
